ba84a7b6207d01419f11004a92761f389489f463
Duy Nguyen <nkduy5920@gmail.com>
2021-11-17 16:17:09 +0700

    update webui

--- StudentManagement/StudentManagement/Models/Student.cs
+++ StudentManagement/StudentManagement/Models/Student.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -9,18 +11,23 @@ namespace StudentManagement.Models
     {
         public int mssv { get; set; }
 
+        [Required(ErrorMessage = "This field cannot be empty!")]
         public string fullname { get; set; }
 
+        [Required(ErrorMessage = "This field cannot be empty!")]
         public string classname { get; set; }
 
         public string sex { get; set; }
 
+        [Required(ErrorMessage ="This field cannot be empty!")]
+        [Range(0,4, ErrorMessage = "GPA must be between 0 and 4!")]
         public double gpa { get; set; }
-
         
 
         public Student()
-        { }
+        {
+
+        }
 
     }
 }
